8272be2e27c0f33e37bf9b470faafc70194e5216
p1ass <me@p1ass.com>
2024-03-23 20:53:39 +0900

    jpgのままで

--- workspaces/app/src/foundation/hooks/useImage.ts
+++ workspaces/app/src/foundation/hooks/useImage.ts
@@ -7,7 +7,7 @@ export const useImage = ({ height, imageId, width }: { height: number; imageId: 
     const dpr = window.devicePixelRatio;
 
     return getImageUrl({
-      format: 'webp',
+      format: 'jpg',
       height: height * dpr,
       imageId,
       width: width * dpr,
