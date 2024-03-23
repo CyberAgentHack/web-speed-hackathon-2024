fa0209dc5c50d2ee7fd72cc698b67f598b2efebb
p1ass <me@p1ass.com>
2024-03-23 20:37:36 +0900

    imageをわざわざcanvasに描画する必要はなさそう

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -3,7 +3,7 @@ import { createMiddleware } from 'hono/factory';
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
 
-  if (c.req.path.includes("/assets")){
+  if (c.req.path.includes("/assets") ||c.req.path.includes("/images")){
     c.res.headers.append('Cache-Control', 'max-age=1000000');
     return
   }
