fcafc037d1ba1a2e6d5a636c09ea9473305cc468
koji.iwase <koji.iwase@prtimes.co.jp>
2024-03-23 17:22:21 +0900

    del jquery

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,6 +1,5 @@
 // import './side-effects';
 
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -15,20 +14,22 @@ const main = async () => {
   await registerServiceWorker();
   // await preloadImages();
 
-  $(document).ready(() => {
-    if (window.location.pathname.startsWith('/admin')) {
-      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
-    } else {
-      ReactDOM.hydrateRoot(
-        $('#root').get(0)!,
-        <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
-          <BrowserRouter>
-            <ClientApp />
-          </BrowserRouter>
-        </SWRConfig>,
-      );
-    }
-  });
+
+  const rootEl = document.getElementById('root');
+
+  if (window.location.pathname.startsWith('/admin')) {
+    ReactDOM.createRoot(rootEl!).render(<AdminApp />);
+  } else {
+    ReactDOM.hydrateRoot(
+      rootEl!,
+      <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
+        <BrowserRouter>
+          <ClientApp />
+        </BrowserRouter>
+      </SWRConfig>,
+    );
+  }
 };
 
 main().catch(console.error);
+
