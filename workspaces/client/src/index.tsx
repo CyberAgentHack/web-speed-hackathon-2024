70072c07304f61110bd2dd0aa2923fab0039a8f8
3846masa <3846masahiro+git@gmail.com>
2024-03-29 15:07:27 +0900

    fix: remove comment slash

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,4 +1,4 @@
-// import './side-effects';
+import './side-effects';
 
 import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
@@ -13,7 +13,7 @@ import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
   await registerServiceWorker();
-  // await preloadImages();
+  await preloadImages();
 
   $(document).ready(() => {
     if (window.location.pathname.startsWith('/admin')) {
