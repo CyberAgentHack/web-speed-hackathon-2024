55ed310a755306da5a6cdd6e6cf877c4b0a0a5fa
UserKazun <Kazu.iosDev@gmail.com>
2024-08-30 14:58:26 +0900

    update support target.

--- workspaces/client/tsup.config.ts
+++ workspaces/client/tsup.config.ts
@@ -60,7 +60,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       shims: true,
       sourcemap: false,
       splitting: false,
-      target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
+      target: ['chrome58'],
       treeshake: false,
     },
   ];
